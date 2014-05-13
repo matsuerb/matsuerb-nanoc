ce43d9375719fcc2316d3c4631fa4a01b02f17a0
Takashi Uchibe <takashi@uchibe.net>
2014-05-13 14:04:41 +0900

    終了時刻の修正

--- commands/create-matsuerb.rb
+++ commands/create-matsuerb.rb
@@ -82,7 +82,7 @@ calendar:
   summary: Matsue.rb定例会H#{nengo}.#{event_date.strftime('%m')}
   description: <a href="#{lative_path}">お知らせページ</a>
   start_time: "9:30"
-  end_time: "17:30"
+  end_time: "17:00"
   location: 島根県松江市朝日町478番地18　松江テルサ別館2階
 ---
 

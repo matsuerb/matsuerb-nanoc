eefdc4f14b057c95d6c1462f9ad06f530ed9427e
Sho Hashimoto <sho.hsmt@gmail.com>
2018-05-05 00:58:25 +0900

    c0f9afa を再適用。

--- static/matrk_common/js/application.js
+++ static/matrk_common/js/application.js
@@ -3,7 +3,11 @@ $(function() {
   //ページ内スクロール
   $("a").click(function () {
     var mark = $(".mark").offset().top;
-    var point = $($(this).attr("href")).offset().top;
+    var id = $(this).attr("href");
+    if (id == '#') {
+      return;
+    }
+    var point = $(id).offset().top;
     $('html,body').animate({ scrollTop: point - 120 }, 'fast');
     return false;
   });

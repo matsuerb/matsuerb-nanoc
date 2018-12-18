76cbeaa368750cb70e311f1f59ecafdc05320ac3
Sho Hashimoto <sho.hsmt@gmail.com>
2018-12-18 15:00:31 +0900

    jQueryの更新。以下に対応。 * JQMIGRATE: jQuery.fn.load() is deprecated

--- static/js/grid/columnset.js
+++ static/js/grid/columnset.js
@@ -19,7 +19,7 @@ new AwesomeGrid('ul.menu', {desktop: 601}).gutters(0).grid(1).desktop(8, 0);
 new AwesomeGrid('ul.column', {desktop: 601}).gutters(0).grid(1).desktop(3, 0);
 new AwesomeGrid('ul.grid', {desktop: 601}).gutters(0).grid(5).desktop(6, 0);
 
-$(window).load(function(){
+$(window).on('load', function(){
   new AwesomeGrid('ul.menu', {desktop: 601}).gutters(0).grid(1).desktop(8, 0);
   new AwesomeGrid('ul.grid', {desktop: 601}).gutters(0).grid(5).desktop(6, 0);
 });

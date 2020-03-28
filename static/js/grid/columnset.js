ae1cf31e3c2e459c588c860895a460300b9136da
Akihiro Sada <sada@netlab.jp>
2020-03-28 16:03:52 +0900

    トップ画面のTwitterのタイムラインを削除

--- static/js/grid/columnset.js
+++ static/js/grid/columnset.js
@@ -16,12 +16,12 @@ var MemberList = (function(){
 MemberList();
 
 new AwesomeGrid('ul.menu', {desktop: 601}).gutters(0).grid(1).desktop(8, 0);
-new AwesomeGrid('ul.column', {desktop: 601}).gutters(0).grid(1).desktop(3, 0);
-new AwesomeGrid('ul.grid', {desktop: 601}).gutters(0).grid(5).desktop(6, 0);
+new AwesomeGrid('ul.column', {desktop: 601}).gutters(0).grid(1).desktop(2, 0);
+new AwesomeGrid('ul.grid', {desktop: 601}).gutters(0).grid(5).desktop(9, 0);
 
 $(window).on('load', function(){
   new AwesomeGrid('ul.menu', {desktop: 601}).gutters(0).grid(1).desktop(8, 0);
-  new AwesomeGrid('ul.grid', {desktop: 601}).gutters(0).grid(5).desktop(6, 0);
+  new AwesomeGrid('ul.grid', {desktop: 601}).gutters(0).grid(5).desktop(9, 0);
 });
 
 var timer = false;
@@ -34,7 +34,7 @@ $(window).on('resize', function() {
     }
     timer = setTimeout(function() {
       MemberList();
-      new AwesomeGrid('ul.grid', {desktop: 601}).gutters(0).grid(5).desktop(6, 0);
+      new AwesomeGrid('ul.grid', {desktop: 601}).gutters(0).grid(5).desktop(9, 0);
     }, 200);
   }
   

99bacde5de2b507b0f0f389676c1783d2aa62e7c
Akihiro Sada <sada@netlab.jp>
2024-01-16 11:55:22 +0900

    awesome-grid.js と columnset.js を削除

--- static/js/grid/columnset.js
+++
@@ -1,8 +0,0 @@
-new AwesomeGrid('ul.menu', {desktop: 601}).gutters(0).grid(1).desktop(6, 0);
-new AwesomeGrid('ul.column', {desktop: 601}).gutters(0).grid(1).desktop(2, 0);
-new AwesomeGrid('ul.grid', {desktop: 601}).gutters(0).grid(5).desktop(9, 0);
-
-$(window).on('load', function(){
-  new AwesomeGrid('ul.menu', {desktop: 601}).gutters(0).grid(1).desktop(6, 0);
-  new AwesomeGrid('ul.grid', {desktop: 601}).gutters(0).grid(5).desktop(9, 0);
-});

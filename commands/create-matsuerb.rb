6782e5593ceff3537b2467b6c0c8e2fa52449ff3
Akihiro Sada <da100ro111@gmail.com>
2026-09-01 06:04:49 +0000

    feat: create-matsuerbコマンドでschedule.htmlの予定行も自動追加
    - create-matsuerbコマンドはお知らせ記事のみを作成しており、content/schedule.htmlへの予定追加は毎回手作業だったため、記事作成後にschedule.htmlへ「参加募集中」状態の行を自動挿入し、別コミットとして積むようにした。
    - イベント番号は既存の(#NNN)表記の最大値+1を自動採番し、Doorkeeper IDは記事作成時と同じ--id/-iオプションを再利用してリンクを生成するようにした。
    - 該当する令和/平成の年セクションが存在しない場合(年またぎで最初の予定を追加する場合)は、直前の年セクションの手前に新しいセクションを作成するようにした。

--- commands/create-matsuerb.rb
+++ commands/create-matsuerb.rb
@@ -25,12 +25,58 @@ def nengo(year, type)
   end
 end
 
+def next_matsuerb_event_number(schedule_content)
+  schedule_content.scan(/\(#(\d+)\)/).map { |m| m[0].to_i }.max + 1
+end
+
+# Inserts a new "参加募集中" row for the given event into content/schedule.html.
+# The row is added to the top of the matching 令和/平成 year section (events are
+# listed in reverse chronological order); if that year has no section yet, a
+# new one is created just before the previous year's section.
+def insert_matsuerb_schedule_row(schedule_content, event_date, event_number, doorkeeper_id)
+  reiwa_year = event_date.year - 2018
+  nengo_en = '%02d' % reiwa_year
+  month2 = event_date.strftime('%m')
+  link =
+    if doorkeeper_id
+      "<%= link_to_doorkeeper('Doorkeeper', 'matsue-rb', #{doorkeeper_id}) %>"
+    else
+      ''
+    end
+  row = "| Matsue.rb定例会R#{nengo_en}.#{month2}(##{event_number})| 参加募集中 | #{event_date.strftime('%Y/%m/%d')} 13:00-17:00 | <%= link_to_osslab %>   | 不要   |無料| #{link} |\n"
+
+  section_header = "## 令和#{reiwa_year}年"
+
+  if schedule_content.include?("#{section_header}\n")
+    section_re = /(#{Regexp.escape(section_header)}\n\n<div markdown="1" class="table_schedule pb-4" >\n\n\|.*\|\n\|[-|]+\|\n)/
+    raise "table header for #{section_header} not found in content/schedule.html" unless schedule_content =~ section_re
+
+    schedule_content.sub(section_re) { $1 + row }
+  else
+    raise 'no existing year section found in content/schedule.html' unless schedule_content =~ /^## (?:令和|平成)/
+
+    new_section = <<~SEC
+      #{section_header}
+
+      <div markdown="1" class="table_schedule pb-4" >
+
+      |イベント名                  |状態          |日時                   |会場                    |事前登録|料金|リンク                      |
+      |----------------------------|--------------|-----------------------|------------------------|--------|----|----------------------------|
+      #{row}
+      </div>
+
+    SEC
+    schedule_content.sub(/^## (?=令和|平成)/) { new_section + '## ' }
+  end
+end
+
 usage 'create-matsuerb EVENT_DATE [options]'
 aliases :cm
 summary 'create news for a periodic Matsue.rb hackathon'
 description <<EOS
 This command create news for a periodic Matsue.rb hackathon in
-content/news/<year>/<month1>/<day>/matsuerb_h<nengo><month2>.html.
+content/news/<year>/<month1>/<day>/matsuerb_h<nengo><month2>.html,
+and adds a corresponding row to content/schedule.html.
 
   <year>, <month1> and <day> are Today or --date(-d) option.
   <nengo> and <month2> are EVENT_DATE option.
@@ -122,6 +168,21 @@ calendar:
   begin
     git.add(relative_path)
     git.commit({m: "#{event_date.month}/#{event_date.day}(#{wday_s[event_date.wday]})のお知らせを追加"}, relative_path)
+  rescue MiniGit::GitError
+    exit(1)
+  end
+
+  schedule_relative_path = 'content/schedule.html'
+  schedule_path = File.expand_path("../../#{schedule_relative_path}", __FILE__)
+  schedule_content = File.read(schedule_path)
+  event_number = next_matsuerb_event_number(schedule_content)
+  updated_schedule_content =
+    insert_matsuerb_schedule_row(schedule_content, event_date, event_number, opts[:id])
+  File.write(schedule_path, updated_schedule_content)
+  puts("update: #{schedule_relative_path}")
+  begin
+    git.add(schedule_relative_path)
+    git.commit({m: "スケジュールに#{event_date.month}/#{event_date.day}(#{wday_s[event_date.wday]})の予定を追加"}, schedule_relative_path)
   rescue MiniGit::GitError
     exit(1)
   end
